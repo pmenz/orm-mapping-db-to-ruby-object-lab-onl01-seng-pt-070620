81ba80560ed17d884b01f85592b8e4861c685253
Pablo Menz <p.menz.r@gmail.com>
2020-10-08 17:24:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
        SELECT *
        FROM students
      SQL
- 
+
      DB[:conn].execute(sql).map do |row|
        self.new_from_db(row)
      end
@@ -33,16 +33,16 @@ class Student
     self.new_from_db(row)
   end.first
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
